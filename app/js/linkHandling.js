c869e85530b98eec68b7e47b2149fdea59235048
Kirk Roberts <4321kirk@gmail.com>
2017-03-22 11:54:12 -0400

    Added CMS Guide and some style tweaks

--- app/js/linkHandling.js
+++ app/js/linkHandling.js
@@ -1,14 +1,27 @@
+
+// external links
 var links = document.getElementsByTagName('a');
 var currentDomain = location.protocol + '//' + location.hostname;
 // console.log(currentDomain);
 for (var i = 0; i < links.length; i++) {
   var link = links[i];
-  // console.log(link);
   var href = link.getAttribute('href');
   if (href &&
     href.startsWith('http') &&
     !href.startsWith(currentDomain)) {
     link.setAttribute('target', '_blank');
     // console.log(link);
+  }
+}
+
+// navigation
+var navLinks = document.querySelectorAll('nav a');
+var currentPath = window.location.pathname;
+for (var i = 0; i < navLinks.length; i++) {
+  var link = navLinks[i];
+  var href = link.getAttribute('href');
+  console.log(href, currentPath);
+  if (href === currentPath) {
+    link.className += 'current';
   }
 }
